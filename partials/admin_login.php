9cb286baec15d905857e1a1cecc0b12cca068eec
mohamed2m2018 <33550035+mohamed2m2018@users.noreply.github.com>
2018-05-05 00:09:32 +0200

    Add files via upload

---
+++ partials/admin_login.php
@@ -0,0 +1,12 @@
+<!--This page is just a part of the register page-->
+<link rel="stylesheet" type="text/css" href="../styles/register.css">
+   
+<div class="log">
+    <h2>I am an admin</h2>
+    <form action="../register.php" method="POST">
+        <input type="email" placeholder="Email" name="admin_email">
+        <input type="password" placeholder="Password" name="admin_password">
+        <input type="submit" value="Enter" name="admin_submit">
+
+    </form>
+</div>
